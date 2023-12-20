b88c1c976a90fd7aef881c1644e463e54f331f61
Braynner Marques <braynnermarques@hotmail.com>
2023-12-19 22:06:51 -0300

    header e footer estilização finalizada

--- frontend/src/components/header/index.jsx
+++ frontend/src/components/header/index.jsx
@@ -1,28 +1,25 @@
 import React from "react";
-import {  Link } from "react-router-dom";
-
+import { Link } from "react-router-dom";
+import "./index.css";
 const Header = () => {
   return (
-    <header>
-      
-      <nav>
-        
-            <Link to="/">Nunes Sports</Link>
-          <ul>
-            <li>
-              <Link to="/list">Lista</Link>
-            </li>
-            <li>
-              <Link to="/new">Cadastro</Link>
-            </li>
-            <li>
-              <Link to="/update">Atualização</Link>
-            </li>
-            <li>
-              <Link to="/delete">Exclusão</Link>
-            </li>
-          </ul>
-        
+    <header className="header">
+      <nav className="menu-bar">
+        <Link className="menu-bar-home" to="/">Nunes Sports</Link>
+        <ul className="menu-bar-links">
+          <li>
+            <Link to="/list">Lista</Link>
+          </li>
+          <li>
+            <Link to="/new">Cadastro</Link>
+          </li>
+          <li>
+            <Link to="/update">Atualização</Link>
+          </li>
+          <li>
+            <Link to="/delete">Exclusão</Link>
+          </li>
+        </ul>
       </nav>
     </header>
   );

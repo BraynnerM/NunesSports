ace62b996901bf4d6a439b282dcce9f6ca876de4
Braynner Marques <braynnermarques@hotmail.com>
2023-12-23 00:06:22 -0300

    corrigindo import

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../prisma/generated/client";
 const port = process.env.PORT ? Number(process.env.PORT) : 4000;
 const app = express();
 const prisma = new PrismaClient();

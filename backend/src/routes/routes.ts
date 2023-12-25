7584df7b04361e0af23c24cd3919fe95be039d22
Braynner Marques <braynnermarques@hotmail.com>
2023-12-25 19:28:37 -0300

    corrigido o routes.ts

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -1,13 +1,13 @@
-// import express from "express";
-// import { getProdutos, getProdutoById, getProdutosByNome, createProduto, updateProduto, deleteProduto } from "../controllers/produtosController"; 
+import express from "express";
+import { getProdutos, getProdutoById, getProdutosByNome, createProduto, updateProduto, deleteProduto } from "../controllers/produtosController"; 
 
-// const router = express.Router();
+const router = express.Router();
 
-// router.get("/produtos/", getProdutos);
-// router.get("/produtos/id/:id", getProdutoById);
-// router.get("/produtos/nome/:nome", getProdutosByNome);
-// router.post("/produtos", createProduto);
-// router.put("/produtos/:id", updateProduto);
-// router.delete("/produtos/:id", deleteProduto);
+router.get("/produtos/", getProdutos);
+router.get("/produtos/id/:id", getProdutoById);
+router.get("/produtos/nome/:nome", getProdutosByNome);
+router.post("/produtos", createProduto);
+router.put("/produtos/:id", updateProduto);
+router.delete("/produtos/:id", deleteProduto);
 
-// export default router;
+export default router;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,8 +4,8 @@ const port = process.env.PORT ? Number(process.env.PORT) : 4000;
 const app = express();
 const prisma = new PrismaClient();
 import cors from 'cors';
-//import router from "./routes/routes"
-import { getProdutos, getProdutoById, getProdutosByNome, createProduto, updateProduto, deleteProduto } from "./controllers/produtosController"; 
+import router from "./routes/routes"
+
 
 const corsOptions = {
   origin: 'https://nunes-sports-frontend.vercel.app',
@@ -15,15 +15,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.use(express.json());
-
-const router = express.Router();
 app.use("/api", router);
-router.get("/produtos/", getProdutos);
-router.get("/produtos/id/:id", getProdutoById);
-router.get("/produtos/nome/:nome", getProdutosByNome);
-router.post("/produtos", createProduto);
-router.put("/produtos/:id", updateProduto);
-router.delete("/produtos/:id", deleteProduto);
 
 app.listen(port, async () => {
   console.log(`Servidor em execução em http://localhost:${port}`);
